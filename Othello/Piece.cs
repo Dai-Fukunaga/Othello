333f11e455fa5fd144f381e9db641729de8359b2
Dai Fukunaga <dai.pochi.700@gmail.com>
2023-06-21 00:08:10 +0900

    make classes

---
+++ Othello/Piece.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Othello
+{
+    internal class Piece
+    {
+        public Player player;
+
+        public int column;
+
+        public int row;
+
+        public Piece(Player player, int column, int row)
+        {
+            this.player = player;
+            this.column = column;
+            this.row = row;
+        }
+    }
+}
---
+++ Othello/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Othello
+{
+    internal class Player
+    {
+        enum Color
+        {
+            BLACK,
+            WHITE,
+        };
+
+        public Player() { }
+
+        public Color other(Color color)
+        {
+            return color == Color.WHITE ? Color.BLACK : Color.WHITE;
+        }
+    }
+}
