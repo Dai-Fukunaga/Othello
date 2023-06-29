366fa202e9d06c14deb26682ec4a1e927f97147d
Dai Fukunaga <dai.pochi.700@gmail.com>
2023-06-29 21:11:33 +0900

    change a label

--- Othello/US_StartMenu.Designer.cs
+++ Othello/US_StartMenu.Designer.cs
@@ -67,9 +67,10 @@ namespace Othello
             // 
             this.radioButton1.AutoSize = true;
             this.radioButton1.Font = new System.Drawing.Font("Yu Gothic UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton1.Location = new System.Drawing.Point(390, 121);
+            this.radioButton1.Location = new System.Drawing.Point(650, 168);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(79, 34);
+            this.radioButton1.Size = new System.Drawing.Size(118, 49);
             this.radioButton1.TabIndex = 0;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Black";
@@ -79,9 +80,10 @@ namespace Othello
             // 
             this.radioButton2.AutoSize = true;
             this.radioButton2.Font = new System.Drawing.Font("Yu Gothic UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton2.Location = new System.Drawing.Point(511, 121);
+            this.radioButton2.Location = new System.Drawing.Point(852, 168);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(86, 34);
+            this.radioButton2.Size = new System.Drawing.Size(129, 49);
             this.radioButton2.TabIndex = 1;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "White";
@@ -90,9 +92,10 @@ namespace Othello
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Othello.Properties.Resources.black;
-            this.pictureBox1.Location = new System.Drawing.Point(390, 156);
+            this.pictureBox1.Location = new System.Drawing.Point(650, 216);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(79, 50);
+            this.pictureBox1.Size = new System.Drawing.Size(132, 69);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
@@ -100,10 +103,10 @@ namespace Othello
             // pictureBox2
             // 
             this.pictureBox2.Image = global::Othello.Properties.Resources.white;
-            this.pictureBox2.Location = new System.Drawing.Point(511, 156);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.pictureBox2.Location = new System.Drawing.Point(852, 216);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(8, 6, 8, 6);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(70, 50);
+            this.pictureBox2.Size = new System.Drawing.Size(117, 69);
             this.pictureBox2.TabIndex = 3;
             this.pictureBox2.TabStop = false;
             this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
@@ -112,10 +115,10 @@ namespace Othello
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(58, 156);
-            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Location = new System.Drawing.Point(97, 216);
+            this.label1.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(254, 40);
+            this.label1.Size = new System.Drawing.Size(387, 60);
             this.label1.TabIndex = 4;
             this.label1.Text = "Choose your Color";
             // 
@@ -123,12 +126,12 @@ namespace Othello
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(58, 251);
-            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Location = new System.Drawing.Point(97, 348);
+            this.label2.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(205, 40);
+            this.label2.Size = new System.Drawing.Size(441, 60);
             this.label2.TabIndex = 5;
-            this.label2.Text = "Choose Enemy";
+            this.label2.Text = "Choose an Opponent";
             // 
             // comboBox1
             // 
@@ -140,15 +143,15 @@ namespace Othello
             "Beginner",
             "Intermediate",
             "Human"});
-            this.comboBox1.Location = new System.Drawing.Point(390, 263);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.comboBox1.Location = new System.Drawing.Point(650, 364);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(8, 6, 8, 6);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(199, 29);
+            this.comboBox1.Size = new System.Drawing.Size(329, 40);
             this.comboBox1.TabIndex = 7;
             // 
             // US_StartMenu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.Controls.Add(this.comboBox1);
@@ -158,9 +161,9 @@ namespace Othello
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.radioButton1);
-            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Margin = new System.Windows.Forms.Padding(8, 6, 8, 6);
             this.Name = "US_StartMenu";
-            this.Size = new System.Drawing.Size(690, 350);
+            this.Size = new System.Drawing.Size(1150, 485);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
